d3b3f249aa9835ac8337fd8f2822b8f9085ef18f
Giovani Martinho do Nascimento <65050113+GiovaniMN@users.noreply.github.com>
2025-05-28 13:44:40 -0300

    Update firebaseConfig.js

--- sistema_de_monitoramento/firebaseConfig.js
+++ sistema_de_monitoramento/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtej6q8PEyBxtzjXQiNIvNoeLGUZpp5VU",
-  authDomain: "testerasp1-fd9ce.firebaseapp.com",
-  projectId: "testerasp1-fd9ce",
-  storageBucket: "testerasp1-fd9ce.appspot.com",
-  messagingSenderId: "859378752124",
-  appId: "1:859378752124:web:8cf3eba575d121700a07f9"
+  apiKey: "APY-KEY",
+  authDomain: "DOMAIN",
+  projectId: "ID",
+  storageBucket: "STORAGE",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID"
 };
 
 const app = initializeApp(firebaseConfig);
